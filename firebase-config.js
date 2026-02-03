9955dddbfb05b241cd96a852182144ea0af726ed
boroda4biz-sys <boroda4.biz@gmail.com>
2026-02-03 23:53:03 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSy...", // Возьми этот ключ в настройках Firebase
+  authDomain: "topgo-courier-new.firebaseapp.com",
+  projectId: "topgo-courier-new",
+  storageBucket: "topgo-courier-new.appspot.com",
+  messagingSenderId: "1090435165651",
+  appId: "1:1090435165651:web:..." // Возьми этот ID в настройках Firebase
+};
